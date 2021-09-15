d3aa32bd1f820ca29414ad78c5b0c384971b0d1d
Daniel <daniel.moderiano@outlook.com>
2021-09-15 10:34:25 +0930

    Convert resume sections to state in main, and render sections using map

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import ResumeSection from "./ResumeSection";
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resumeSections: [
+        'Summary',
+        'Experience',
+        'Education',
+        'Key Skills',
+        'Interests'
+      ]
+    };
+  }
+
   render() {
     return (
       <main className="main">
-        <ResumeSection title="Summary" />
-        <ResumeSection title="Experience" />
-        <ResumeSection title="Education" />
-        <ResumeSection title="Key Skills" />
-        <ResumeSection title="Interests" />
+        {this.state.resumeSections.map((section) => (<ResumeSection title={section} />))}
       </main>
     );
   }
