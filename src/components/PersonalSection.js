c561e3820f5b4f6cfa89bd244350994f28a725ae
Daniel <daniel.moderiano@outlook.com>
2021-09-21 15:22:06 +0930

    Convert PersonalSection.js to functional component

--- src/components/PersonalSection.js
+++ src/components/PersonalSection.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class PersonalSection extends Component {
-  render() {
-    return (
-      <div className="personal">
-        <div className="header">
-          <h1 className="personal__name">{this.props.data.firstName} {this.props.data.lastName}</h1>
-          <h2 className="personal__title">{this.props.data.currentRole}</h2>
-        </div>
-        <div className="personal__info">
-          <div className="personal__address-one">{this.props.data.addressOne}</div>
-          <div className="personal__address-two">{this.props.data.addressTwo}</div>
-          <div className="personal__phone">{this.props.data.phone}</div>
-          <div className="personal__email">{this.props.data.email}</div>
-          <div className="personal__linkedin">{this.props.data.linkedin}</div>
-        </div>
-        <div className="summary">
-          <h2 className="summary__title">Summary</h2>
-          <p className="summary__text">{this.props.data.summary}</p>
-        </div>
+const PersonalSection = (props) => {
+  const {firstName, lastName, currentRole, addressOne, addressTwo, phone, email, linkedin, summary} = props.data;
+
+  return (
+    <div className="personal">
+      <div className="header">
+        <h1 className="personal__name">{firstName} {lastName}</h1>
+        <h2 className="personal__title">{currentRole}</h2>
       </div>
-      
-    );
-  }
+      <div className="personal__info">
+        <div className="personal__address-one">{addressOne}</div>
+        <div className="personal__address-two">{addressTwo}</div>
+        <div className="personal__phone">{phone}</div>
+        <div className="personal__email">{email}</div>
+        <div className="personal__linkedin">{linkedin}</div>
+      </div>
+      <div className="summary">
+        <h2 className="summary__title">Summary</h2>
+        <p className="summary__text">{summary}</p>
+      </div>
+    </div>
+  );
 }
+
+export default PersonalSection;
