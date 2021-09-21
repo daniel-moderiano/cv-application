4b9ebdd862b5240cba9002c12d2e025c95b7bb90
Daniel <daniel.moderiano@outlook.com>
2021-09-21 15:12:33 +0930

    Convert App.js to functional compoonent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Main from "./components/Main";
 import '../src/styles/reset.css';
 import '../src/styles/app.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
+const App = () => {
+  return (
+    <div className="app">
       <header className="header">
         <h1 className="header__title">CV Generator</h1>
       </header>
       <Main />
     </div>
-    );
-  };
+  );
 }
 
-export default App;
+export default App
+
